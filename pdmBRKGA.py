b9c6354a6c2d0cb604527f318354c6cfa64cf0c1
Diogo Leite <diogoleite87@gmail.com>
2022-07-23 22:14:51 -0300

    add lista adj e melhoria custo computacional

--- pdmBRKGA.py
+++ pdmBRKGA.py
@@ -11,6 +11,7 @@ class PdmBRKGA:
     def addEdge(self, u, v, result):
         if u < self.num_vet and v < self.num_vet:
             self.mat_adj[u][v] = result
+            self.lista_adj[u].append(result)
         else:
             print("Aresta Invalida")
 
@@ -25,29 +26,36 @@ class PdmBRKGA:
 
     def firstStep(self, fileCSV):
 
-        self.mat_adj = [[0 for _ in range(len(fileCSV))]
-                        for _ in range(len(fileCSV))]
         self.num_vet = len(fileCSV)
+        self.mat_adj = [[0 for _ in range(self.num_vet)]
+                        for _ in range(self.num_vet)]
+        self.lista_adj = [[] for i in range(self.num_vet)]
 
         for i in range(len(fileCSV)):
             for j in range(len(fileCSV)):
 
-                result = math.sqrt((
-                    (float(fileCSV['valence'][i]) - (float(fileCSV['valence'][j])))**2)
-                    + ((float(fileCSV['acousticness'][i]) -
-                        (float(fileCSV['acousticness'][j])))**2)
-                    + ((float(fileCSV['danceability'][i]) -
-                        (float(fileCSV['danceability'][j])))**2)
-                    + ((float(fileCSV['energy'][i]) -
-                        (float(fileCSV['energy'][j])))**2)
-                    + ((float(fileCSV['instrumentalness'][i]) -
-                        (float(fileCSV['instrumentalness'][j])))**2)
-                    + ((float(fileCSV['liveness'][i]) -
-                        (float(fileCSV['liveness'][j])))**2)
-                    + ((float(fileCSV['speechiness'][i]) -
-                        (float(fileCSV['speechiness'][j])))**2))
+                if i == j:
+                    result = 0.0
+                    self.addEdge(i, j, result)
 
-                self.addEdge(i, j, result)
+                elif self.mat_adj[i][j] == 0:
+                    result = math.sqrt((
+                        (float(fileCSV['valence'][i]) - (float(fileCSV['valence'][j])))**2)
+                        + ((float(fileCSV['acousticness'][i]) -
+                            (float(fileCSV['acousticness'][j])))**2)
+                        + ((float(fileCSV['danceability'][i]) -
+                            (float(fileCSV['danceability'][j])))**2)
+                        + ((float(fileCSV['energy'][i]) -
+                            (float(fileCSV['energy'][j])))**2)
+                        + ((float(fileCSV['instrumentalness'][i]) -
+                            (float(fileCSV['instrumentalness'][j])))**2)
+                        + ((float(fileCSV['liveness'][i]) -
+                            (float(fileCSV['liveness'][j])))**2)
+                        + ((float(fileCSV['speechiness'][i]) -
+                            (float(fileCSV['speechiness'][j])))**2))
+
+                    self.addEdge(i, j, result)
+                    self.addEdge(j, i, result)
 
     def run(self, nameFIle):
         self.firstStep(self.readFile(nameFIle))
